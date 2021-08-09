d5022c9c8765fe6f2a0946b9509c2de1ecc36d74
Fl0-dev <florian.girard.dev@outlook.fr>
2021-08-09 16:30:02 +0200

    gestion rôles

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -82,16 +82,21 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getRoles(): array
     {
+        /* si pas de gestion de rôles
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
+        */
+        return $this->roles;
     }
 
     public function setRoles(array $roles): self
     {
         $this->roles = $roles;
+
+
 
         return $this;
     }
