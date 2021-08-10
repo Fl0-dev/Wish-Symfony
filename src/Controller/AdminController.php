ce09cdcbba410835273830135d5d0760f387628f
Fl0-dev <florian.girard.dev@outlook.fr>
2021-08-10 12:19:09 +0200

    Affichage des users sur le dashboard

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,10 +15,11 @@ class AdminController extends AbstractController
     /**
      * @Route("/dashboard", name="dashboard")
      */
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
+        $users= $userRepository->findAll();
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'users' => $users,
         ]);
     }
 }
